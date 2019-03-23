d07eb1be6f11536bf2e542f90c461c6b8933bec3
Shajedul Islam <shajedul.islam@naztech.us.com>
2019-03-24 01:56:05 +0600

    Project structure created with basic home page

---
+++ src/components/elements/HeroImage/HeroImage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './HeroImage.css';
+
+const HeroImage = (props) => {
+    return (
+        <div>
+            HeroImage
+        </div>
+    )
+}
+export default HeroImage
